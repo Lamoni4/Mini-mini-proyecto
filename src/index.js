be2abafa788f61fd8f5482715e55097bd6d8f655
Lamoni4 <alexpinchi37@gmail.com>
2024-07-03 18:56:31 -0400

    limpiar archivos residuales

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ const server = http.createServer(async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto http://localhost/${PORT}`);
+    console.log(`Servidor levantado en el puerto http://localhost/${PORT}`);
 });
